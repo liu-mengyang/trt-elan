6dd8fe76e5c95e3f4932548cbb03d6f270abff70
Mengyang Liu <myliu@seu.edu.cn>
2022-06-10 22:05:42 +0800

    Init unit test of reflect padding plugin

---
+++ src/reflectpad/parse.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+trtexec \
+        --onnx=unit.onnx \
+        --explicitBatch \
+        --minShapes=lr:1x3x64x64 \
+        --optShapes=lr:1x3x80x80 \
+        --maxShapes=lr:1x3x120x120 \
+        --saveEngine=unit.plan \
+        --workspace=40960 \
+        --buildOnly \
+        --noTF32 \
+        --verbose \
---
+++ src/reflectpad/unit.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class ReflectPad(nn.Module):
+    def __init__(self):
+        super(ReflectPad, self).__init__()
+
+    def forward(self, input):
+        out = F.pad(input, (0, 1, 0, 2), "reflect")
+        return out
+    
+input = torch.arange(9, dtype=torch.float).reshape(1, 1, 3, 3).cuda()
+print(input)
+rp = ReflectPad().cuda()
+out = rp(input)
+print(out)
+
+torch.onnx.export(rp,
+                  input,
+                  "unit.onnx",
+                  input_names=["input"],
+                  output_names=["output"],
+                  verbose=True,
+                  keep_initializers_as_inputs=True,
+                  opset_version=13,
+                  dynamic_axes={"input": {0: "batch_size"}})
