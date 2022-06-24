9c9994f45bc11d85c9681b796c44d8fdc0a52a7c
yindaheng98 <yindaheng98@gmail.com>
2022-06-24 17:09:35 +0800

    all in, but failed

--- src/parse_onnx_all.py
+++ src/parse_onnx_all.py
@@ -17,13 +17,12 @@ network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPL
 profile = builder.create_optimization_profile()
 config = builder.create_builder_config()
 config.flags = config.flags | 1 << int(trt.BuilderFlag.FP16) | 1 << int(trt.BuilderFlag.DEBUG)
+config.set_flag(trt.BuilderFlag.FP16)
+config.set_flag(trt.BuilderFlag.DEBUG)
+config.set_flag(trt.BuilderFlag.STRICT_TYPES)
 config.max_workspace_size = 3 << 30
 config.profiling_verbosity = trt.ProfilingVerbosity.DETAILED
 parser = trt.OnnxParser(network, logger)
-if not os.path.exists(onnxFile):
-    print("Failed finding onnx file!")
-    exit()
-print("Succeeded finding onnx file!")
 with open(onnxFile, 'rb') as model:
     if not parser.parse(model.read()):
         print("Failed parsing .onnx file!")
@@ -32,15 +31,19 @@ with open(onnxFile, 'rb') as model:
         exit()
     print("Succeeded parsing .onnx file!")
 
-lr = network.get_input(0)
-
 total = 0
 for layer in network:
+    if layer.precision != trt.DataType.FLOAT:
+        continue
     if layer.type in [trt.LayerType.CONVOLUTION, """trt.LayerType.ELEMENTWISE""", trt.LayerType.MATRIX_MULTIPLY]:
         total += 1
-        print(total, layer.type, layer.precision, layer.precision_is_set)
+        layer.precision = layer.precision
+        print(total, layer.name, layer.type, layer.precision, layer.precision_is_set)
+    else:
         layer.precision = trt.DataType.HALF
-        print(total, layer.type, layer.precision, layer.precision_is_set)
+        print(total, layer.name, layer.type, layer.precision, layer.precision_is_set)
+
+lr = network.get_input(0)
 
 profile.set_shape(lr.name, (1, 3, 304, 208), (1, 3, 304, 208), (1, 3, 304, 208))
 config.add_optimization_profile(profile)
